cc693ec67f88dc8f9c7651edb45445e86fad0184
gwKim <kkwkjm4231@gmail.com>
2023-12-18 14:47:03 +0900

    fix: 로그인 모달 닫기 버튼 누락 수정

--- src/app/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -2,13 +2,19 @@
 
 import {useState} from "react";
 import style from '@/app/(beforeLogin)/_component/login.module.css'
+import {useRouter} from "next/navigation";
 
 export default function LoginModal() {
   const [id, setId] = useState();
   const [password, setPassword] = useState();
   const [message, setMessage] = useState();
+  const router = useRouter();
+
   const onSubmit = () => {};
-  const onClickClose = () => {};
+  const onClickClose = () => {
+    router.back();
+    // TODO: 뒤로가기가 /home이 아니면 /home으로 보내기
+  };
 
   const onChangeId = () => {};
 
