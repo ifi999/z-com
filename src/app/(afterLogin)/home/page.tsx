3a8a9186ec085f035a704a8f4b8170853d2774e7
ifi999 <kkwkjm4231@gmail.com>
2023-12-19 21:36:53 +0900

    add: Context API 적용 - TabProvider

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,19 +2,22 @@ import style from './home.module.css'
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 import Post from "@/app/(afterLogin)/_component/Post";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 
 
 export default function Home() {
   return (
     <main className={style.main}>
-      <Tab />
-      <PostForm />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
     </main>
   )
 }
